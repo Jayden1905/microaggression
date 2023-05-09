f64e5eb8b6aba44247a14047d82c7b0c4e044c74
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-05-09 16:25:07 +0800

    fixing seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html data-theme='night'>
+    <Html data-theme="night">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
